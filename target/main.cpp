57bcad9d3253cad0bea7ed8455f1eac523e77b4d
htn4179 <30617294+htn4179@users.noreply.github.com>
2018-07-03 21:08:13 +0800

    test

--- target/main.cpp
+++ target/main.cpp
@@ -7,8 +7,9 @@ int main()
 {
 	int i = 0;
 	while (1) {
-		printf("Hello,ptrace! [pid:%d]! num is %d\n", getpid(), i++);
+		printf("target process,ptrace! [pid:%d]! num is %d\n", getpid(), i++);
 		sleep(2);
 	}
+	
 	return 0;
 }
